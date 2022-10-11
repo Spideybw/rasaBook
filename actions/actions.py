5c9e1771256f616e72346c371ca9a516460ba6fa
Spideybw <1357037897@qq.com>
2022-10-11 23:48:26 +0800

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,124 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+import sqlite3
+from typing import Any, Text, Dict, List
+
+# import arrow 
+# import dateparser
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+conn = sqlite3.connect('rasa.db')
+c = conn.cursor() 
+
+class queryActivityInfor(Action):
+
+    def name(self) -> Text:
+        return "action_activity_information"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        activity_name =  tracker.get_slot("activity_name")  
+        print(activity_name)
+        # conn = sqlite3.connect('rasa.db')
+        c = conn.cursor()   
+        c.execute("SELECT * from activity WHERE name = ?",(activity_name,))
+        result = c.fetchone()
+        print(result)
+        if result != None:
+            dispatcher.utter_message(text=f"Activity name: {result[0]} \n start date: \n ticket price {result[2]} \n organizer: {result[3]} \n description: {result[4]} \n")
+        else:
+            dispatcher.utter_message(text=f"{activity_name} not exist")
+        c.close()
+
+        # dispatcher.utter_message(text="Hello World!")
+
+        return [SlotSet("activity_name",None)]
+
+class quaryUser(Action):
+
+    def name(self) -> Text:
+        return "initializing_user"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        username =  tracker.get_slot("user_name")  
+        print(username)
+        # conn = sqlite3.connect('rasa.db')
+        c = conn.cursor()   
+        c.execute("SELECT * from customer WHERE name = ?",(username,))
+        result = c.fetchone()
+        print(result)
+        if result != None:
+            dispatcher.utter_message(text=f"Hello {username}")
+        else:
+            c.execute("INSERT INTO customer(name,gender,STU_EMAIL) VALUES(?,?,?)",(str(username),int(1),"123@gmail.com"))
+            # conn.commit()
+            dispatcher.utter_message(text=f"you are a new user! We set up an account for you. your name is {username}")
+        conn.commit()
+        c.close()
+
+        return []
+
+
+class quaryAllActivity(Action):
+
+    def name(self) -> Text:
+        return "action_activity_list"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        # conn = sqlite3.connect('rasa.db')
+        c = conn.cursor()   
+        c.execute("SELECT name from activity")
+        result = c.fetchall()
+        print(result[0])
+        res = "activity name list:"
+
+        for re in result:  
+            res = res + "\n" + str(re[0])
+
+        print(res)
+        dispatcher.utter_message(text=res)
+        c.close()
+
+        return []
+
+class bookActivity(Action):
+
+    def name(self) -> Text:
+        return "action_book_activity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        activity_name =  tracker.get_slot("activity_name")  
+        print(activity_name)
+        user_name =  tracker.get_slot("user_name")  
+        print(user_name)
+        c = conn.cursor()   
+        c.execute("SELECT * from activity WHERE name = ?",(activity_name,))
+        result = c.fetchone()
+        print(result)
+        if result != None:
+            c.execute("INSERT INTO book(activity_name,user_name) VALUES(?,?)",(activity_name,user_name))
+            
+            dispatcher.utter_message(text=f"The subscription to activity {activity_name} succeeded")
+        else:
+            dispatcher.utter_message(text=f"{activity_name} not exist")
+        conn.commit()
+        c.close()
+
+        return []
+
---
+++ actions/rasa.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+
+# # class ActionHelloWorld():
+
+#     # def name(self) -> Text:
+#     #     return "action_activity_information"
+
+# def run():
+# conn = sqlite3.connect('rasa.db')
+# c = conn.cursor()   
+# username = "ybw"
+# print(username)
+# c.execute("INSERT INTO user(name,gender,STU_EMAIL) VALUES(?,?,?)",(str(username),int(1),"123@gmail.com"))
+# print("success")
+# conn.commit()
+# c.close()
+#         # print(activity_name)
+#         conn = sqlite3.connect('rasa.db')
+#         c = conn.cursor()   
+#         c.execute("SELECT * from activity WHERE name = ?",("AAActivity",))
+#         result = c.fetchone()
+#         # print(result)
+#         if result != None:
+#           print("Activity name:" + result[0] + "\n" + 
+#                                         "start date:" + "\n" +
+#                                         "ticket price" + str(result[2]) + "\n" +
+#                                         "organizer:" + result[3] + "\n"
+#                                         "description:" + result[4] + "\n")
+#         #   dispatcher.utter_message(text="Activity name:" + result[0] + "\n" + 
+#         #                                 "start date:" + "\n" +
+#         #                                 "ticket price" + result[2] + "\n" +
+#         #                                 "organizer:" + result[3] + "\n"
+#         #                                 "description:" + result[4] + "\n")
+#         #   print()
+#         else:
+#             # dispatcher.utter_message(text="not exist")
+#             print("nononono")
+#         c.close()
+
+
+# run()
+#         # dispatcher.utter_message(text="Hello World!")
+
+#         # return []
+
+conn = sqlite3.connect('rasa.db')
+c = conn.cursor()   
+c.execute("INSERT INTO book(activity_name,user_name) VALUES(?,?)",("AAActivity","ybw"))
+print(1)
+conn.commit()
+# result = c.fetchall()
+# print(result[0])
+# res = "activity name list:"
+# for re in result:  
+#     res = res + "\n" + str(re[0])
+# print(res)
